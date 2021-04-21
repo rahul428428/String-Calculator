9afbb6e21770131bd834343600cf35a7d480688d
rahul428428 <1996meenarahul@gmail.com>
2021-04-21 10:40:30 +0530

    Refactor the code

--- String Calculator/src/StringCalculator.java
+++ String Calculator/src/StringCalculator.java
@@ -2,18 +2,25 @@
 public class StringCalculator {
 
 	public int Add(String numbers){
+	 
+		return(getSum(numbers));
 	
-	String[]  integers = numbers.split(",");
+	}
+	
+	
+	public int getSum(String str){
 		
-	 if(numbers.isEmpty())	
-		return 0;  
-	 else{
-		int sum=0 ;
-		  for(String curr:integers){
-			  
-			  sum += Integer.parseInt(curr);			
-	     	}
-		return sum; 
-	 }
+		String[]  integers = str.split(",");
+		
+		 if(str.isEmpty())	
+			return 0;  
+		 else{
+			int sum=0 ;
+			  for(String curr:integers){
+				  
+				  sum += Integer.parseInt(curr);			
+		     	}
+			return sum; 
+		 }
 	}
 }
